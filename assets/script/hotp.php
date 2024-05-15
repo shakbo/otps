e1e76fd883894816436b9e6f96e0d1343b8fb992
shakbo <speciers63@gmail.com>
2024-05-15 08:10:43 +0800

    Implementing hotp for future use

---
+++ assets/script/hotp.php
@@ -0,0 +1,19 @@
+<?php
+function hmac($key, $message, $hash, $blockSize, $outputSize) {
+    $block_sized_key = computeBlockSizedKey($key, $hash, $blockSize);
+    $o_key_pad = $block_sized_key ^ str_repeat(chr(0x5c), $blockSize);
+    $i_key_pad = $block_sized_key ^ str_repeat(chr(0x36), $blockSize);
+    return $hash($o_key_pad . $hash($i_key_pad . $message));
+}
+
+function computeBlockSizedKey($key, $hash, $blockSize) {
+    if (strlen($key) > $blockSize) {
+        $key = $hash($key);
+    }
+    if (strlen($key) < $blockSize) {
+        return str_pad($key, $blockSize, "\0");
+    }
+    return $key;
+}
+
+?>
