3d2ad9e23a929c9ec424644d8d188b1c1d16003e
shakbo <speciers63@gmail.com>
2024-05-15 08:09:21 +0800

    Initialized definition for email service

---
+++ configs/email.php
@@ -0,0 +1,9 @@
+<?php
+define('MAIL_HOST', '');
+define('MAIL_USERNAME', '');
+define('MAIL_PASSWORD', '');
+define('MAIL_SEND_FROM', '');
+define('MAIL_SEND_FROM_NAME', '');
+define('MAIL_REPLY_TO', '');
+define('MAIL_REPLY_TO_NAME', '');
+?>
