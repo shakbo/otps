db9c73fdeff57ae5f2ffd47358589db7e151fefc
shakbo <speciers63@gmail.com>
2024-05-16 14:42:36 +0800

    Implement the sign-up/login features.

---
+++ assets/script/logout.php
@@ -0,0 +1,13 @@
+<?php
+include_once($_SERVER['DOCUMENT_ROOT'].'/otps/configs/general.php');
+
+session_start();
+
+if(!empty($_SESSION['SES']))
+{
+	unset($_SESSION['SES']);
+	setcookie('SES','',time()-(9000));
+	
+}
+
+header("Location: " . ROOT . 'index.php');
--- assets/script/signUpValidate.php
+++ assets/script/signUpValidate.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 $sqlConnection = require_once($_SERVER['DOCUMENT_ROOT'].'/otps/configs/database.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/otps/assets/script/miscellaneous.php');
 
@@ -14,7 +16,7 @@ if(!($_SERVER['REQUEST_METHOD'] == 'POST')) {
 $sqlCommand = "SELECT * FROM users WHERE email ='".$email."'";
 $result = mysqli_query($sqlConnection, $sqlCommand);
 
-if(!($result)) {
+if(mysqli_num_rows($result) > 0) {
     alert("此電子郵件已有人使用！");
     die();
 }
@@ -23,11 +25,13 @@ if(!($result)) {
 
 $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
-$sqlCommand = "INSERT INTO keypairs(key) VALUES(NULL); SET @last_keypairs_id = LAST_INSERT_ID();";
+$sqlCommand = "INSERT INTO keypairs(id, hotp, counter, totp) VALUES(NULL, NULL, NULL, NULL)";
+mysqli_query($sqlConnection, $sqlCommand);
 
-$sqlCommand = "INSERT INTO users(id, username, password, email, phoneNumber, token_value, token_key, keypairs, accesslevel) VALUES(NULL, '$username', '$hashedPassword', '$email', NULL, NULL, NULL, NULL, '1')";
+$last_keypairs_id = mysqli_insert_id($sqlConnection);
+
+$sqlCommand = "INSERT INTO users(id, username, password, email, phoneNumber, token_value, token_key, keypairs, accesslevel) VALUES(NULL, '$username', '$hashedPassword', '$email', NULL, NULL, NULL, $last_keypairs_id, '2')";
 $result = mysqli_query($sqlConnection, $sqlCommand);
-
 if($result) {
     alert("註冊成功！");
     die();
