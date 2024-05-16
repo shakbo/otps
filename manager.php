b918d6214c8869653ac1e172d9e0688c58d19d29
shakbo <speciers63@gmail.com>
2024-05-16 14:37:09 +0800

    split page control into two file to fix ERR_TOO_MANY_REDIRECTS.

--- manager.php
+++ manager.php
@@ -8,12 +8,25 @@
     <script src="assets/library/bootstrap/bootstrap.bundle.js"></script>
     <script src="assets/library/jquerry/jquery-3.7.1.js"></script>
     <script src="assets/library/jbvalidator/jbvalidator.js"></script>
-    <link rel="stylesheet" type="text/css" href="assets/stylesheet/general.css">
-    <link rel="stylesheet" type="text/css" href="assets/stylesheet/index.css">
     <link rel="stylesheet" type="text/css" href="assets/stylesheet/modal.css">
-    <title>OTP 展示 | 首頁</title>
     <?php
     require_once($_SERVER['DOCUMENT_ROOT'].'/otps/assets/script/miscellaneous.php');
+    
+    $current_page = isset($_GET['page']) ? $_GET['page'] : 'home';
+
+    $page_data = [
+        'home' => [
+            'title' => 'OTP 展示 | 個人首頁',
+            'stylesheet' => 'assets/stylesheet/home.css',
+            'content' => 'assets/content/home.php',
+        ]
+    ];
+
+    if (array_key_exists($current_page, $page_data)) {
+        $page = $page_data[$current_page];
+        echo '<title>' . $page['title'] . '</title>';
+        echo '<link rel="stylesheet" type="text/css" href="' . $page['stylesheet'] . '">';
+    }
     ?>
 </head>
 <body>
@@ -23,7 +36,13 @@
     ?>
     <div class="background"></div>
     <main>
-        <?php include_once($_SERVER['DOCUMENT_ROOT'].'/otps/assets/content/index.php'); ?>
+    <?php
+    if (array_key_exists($current_page, $page_data)) {
+        include_once($page['content']);
+    } else {
+        echo "<h1>Page not found</h1>";
+    }
+    ?>
     </main>
 </body>
 </html>
