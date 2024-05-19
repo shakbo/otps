614ae33e43344f20f0915ae97d2e1ddba37e1ff5
shakbo <speciers63@gmail.com>
2024-05-19 14:43:06 +0800

    Added pages for about website and creator.

---
+++ assets/content/about/creator.php
@@ -0,0 +1,12 @@
+<div class="content flex-column">
+    <div class="description text-end">
+        <p class="h1">網站製作者：宋仁風</p>
+        <p class="h1">就讀於 <a href="https://www.nkust.edu.tw/" class="school">國立高雄科技大學</a> <a href="http://bc.nkust.edu.tw/Home/Index#全部公告" class="department">商務資訊應用系</a></p>
+        <p class="h1">學號為 C111193224</p>
+        <a class="btn btn-light backtoaboutpage" href="index.php?page=about" role="button">返回上頁</a>
+    </div>
+    <div class="accountsecureimage">
+        <!-- 這裡應該要放個人圖片 -->
+        <!-- <img src="assets/image/" alt="" width="auto" height="384"> -->
+    </div>
+</div>
---
+++ assets/content/about/website.php
@@ -0,0 +1,6 @@
+<div class="content flex-column">
+    <div class="description text-end">
+        <p class="h1">該網站(OTPs)由 <a href="index.php?page=aboutCreator" class="creator-name px-2">宋仁風</a> 製作</p>
+        <p class="h1">旨在提供完整功能網站的程式碼</p>
+    </div>
+</div>
