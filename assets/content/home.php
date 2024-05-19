ee86091b7a7c595b659bf155807a726a9369f1e1
shakbo <speciers63@gmail.com>
2024-05-19 14:41:59 +0800

    Rename file for new page manager.

--- assets/content/home.php
+++ assets/content/home.php
@@ -1,3 +1,10 @@
-<p class="text-center">
-  <p class="h1">Home page after logged in.</p>
-</p>
+<div class="content">
+    <div class="description">
+        <p class="h1">OTPs 更直觀的展示了</p>
+        <p class="h1">一次性密碼方式的不同</p>
+    </div>
+    <div class="accountsecureimage">
+        <img src="assets/image/protection.png" alt="userShield" width="auto" height="384" class="usershield">
+        <img src="assets/image/cogwheel.png" alt="cogwheel" width="auto" height="192" class="cogwheel">
+    </div>
+</div>
