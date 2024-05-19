cfac4cd6fe73868452f7a5c930f493e28d9ce1a7
shakbo <speciers63@gmail.com>
2024-05-19 14:46:26 +0800

    Fix logout not actually logout problem.

--- assets/script/logout.php
+++ assets/script/logout.php
@@ -3,10 +3,12 @@ include_once($_SERVER['DOCUMENT_ROOT'].'/otps/configs/general.php');
 
 session_start();
 
-if(!empty($_SESSION['SES']))
+if(!empty($_SESSION['SES']) || !empty($_SESSION['loggedin']))
 {
 	unset($_SESSION['SES']);
 	setcookie('SES','',time()-(9000));
+	$_SESSION = array(); 
+	session_destroy(); 
 }
 
 header("Location: " . ROOT . 'index.php');
